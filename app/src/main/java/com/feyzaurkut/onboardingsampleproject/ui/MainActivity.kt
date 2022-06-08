3c6f1a5ac81b38ab638f9e6ef4059c6eb1864ff5
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-06-08 23:22:34 +0300

    create blank fargments

--- app/src/main/java/com/feyzaurkut/onboardingsampleproject/ui/MainActivity.kt
+++ app/src/main/java/com/feyzaurkut/onboardingsampleproject/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.feyzaurkut.onboardingsampleproject
+package com.feyzaurkut.onboardingsampleproject.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.feyzaurkut.onboardingsampleproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/feyzaurkut/onboardingsampleproject/ui/onboarding/OnboardingFragment.kt
@@ -0,0 +1,20 @@
+package com.feyzaurkut.onboardingsampleproject.ui.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.feyzaurkut.onboardingsampleproject.R
+
+class OnboardingFragment : Fragment() {
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+    }
+    
+}
