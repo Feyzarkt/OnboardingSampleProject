8f9ada3ae16b36347a903eaa9680cfd923ba0afa
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-06-09 00:00:17 +0300

    edit splash ui

--- app/src/main/java/com/feyzaurkut/onboardingsampleproject/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/feyzaurkut/onboardingsampleproject/ui/splash/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.feyzaurkut.onboardingsampleproject.ui.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.feyzaurkut.onboardingsampleproject.R
 
 class SplashFragment : Fragment() {
@@ -13,7 +16,17 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
+        //val isVisited = context?.let { SharedPref(it).getBooleanVisitOnBoarding() }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_onboardingFragment)
+            /* if(isVisited == true)
+                 observeDatabase()
+             else
+                 findNavController().navigate(SplashScreenFragmentDirections.actionSplashScreenFragmentToOnBoardingFragment())
+         */}, 3000)
+
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
